fd90ccf90d299f95cddc581098502260725d80d9
Simon Hischier <simon_hischier@hotmail.com>
2024-02-05 16:31:50 +0100

    WIP routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { StudentGithubPageComponent } from './shared/student-github-page/student-github-page.component';
+import { ExampleAComponent } from './shared/example-a/example-a.component';
+import { AppComponent } from './app.component';
+import { ComponentsIntroductionComponent } from './components-introduction/components-introduction.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ComponentsIntroductionComponent
+  },
+  {
+    path: 'student-a',
+    component: ExampleAComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'student-a',
+    pathMatch: 'full'
+  },
+];
